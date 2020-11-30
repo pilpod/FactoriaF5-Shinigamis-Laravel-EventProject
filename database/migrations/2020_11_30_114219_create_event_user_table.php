792cad879f563e43856eb2fe9153e4f6467399c0
Jackson Garcia <Jacksongp1405@gmail.com>
2020-11-30 13:02:34 +0100

    Creacion de tabla pivot en migrations

---
+++ database/migrations/2020_11_30_114219_create_event_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigIntenger(id_user);
+            $table->foreign("id_user")->references("id")->on("users")->onDelete("set null");
+            $table->unsignedBigIntenger(id_event);
+            $table->foreign("id_event")->references("id")->on("events")->onDelete("set null");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_user');
+    }
+}
