d41160ab6cfeda92a1bb9749c9506e72905cacdc
Alvaro <astaboada7@gmail.com>
2020-12-10 15:34:53 +0100

    still searching, it's become a hell

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,9 +10,10 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="{{asset('templateHtml/css/main.css')}}" />
-		<link rel="stylesheet" href="{{asset('templateHtml/css/slideshow.css')}}" />
+		<!-- <link rel="stylesheet" href="{{asset('templateHtml/css/slideshow.css')}}" /> -->
 		<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 		<link href="{{asset('css/app.css')}}" rel="stylesheet">
+		<link href="{{asset('css/slider.css')}}" rel="stylesheet"><!-- acuerdate de quitarlo cabron!! -->
 </head>
 
 	<body class="homepage is-preload">
@@ -53,56 +54,81 @@
 				<section>
 					<!-- <div class"container-fluid"> -->
 						
-						<div class="slider">
+						<!-- <div class="slider">
 							@foreach($outstanding as $item)
 								<div class="slide" id="slide">
 									<p>{{$item->title}}</p>
-									<img src="{{$item->picture_path}}" class="img">
+									<img src="{{$item->picture_path}}" class="img" alt="">
 									<p>{{"$item->short_description"}}</p>
-									<ol class="carousel-indicators">
+								</div>
+							@endforeach
+						
+						</div>	
+						<div>
+							<ol class="carousel-indicators">
+								@foreach($outstanding as $item)		
 										<li class="inline-block mr-3">
 										<label class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
 										</li>
-									</ol>
-								</div>
-							@endforeach		
-						</div>
+										@endforeach		
+							</ol>
+						</div> -->
 							
-					<!-- <div class="carousel relative shadow-2xl bg-white">
-						<div class="carousel-inner relative overflow-hidden w-full"> -->
+					<div class="carousel relative shadow-2xl bg-white">
+						<div class="carousel-inner relative overflow-hidden w-full">
 						<!--Slide 1-->
-						<!-- @foreach($outstanding as $item)
+						
 							<input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
-							<div class="carousel-item absolute opacity-0" style="height:50vh;">
-								<div class="block h-full w-full bg-indigo-500 text-white text-5xl text-center">{{$item->title}}</div>
+							<div class="carousel-item absolute opacity-0 bg-cover" style="height:50vh;">
+								<div class="block h-full w-full bg-indigo-500 text-white text-5xl text-center">			
+									
+									<!-- <div class="z-10"><p>{{$outstanding[0]->title}}</p></div> -->
+									<div class="z-0"><img src="{{$outstanding[0]->picture_path}}" class="object-none w-full" alt=""></div>
+									
+								</div>
 							</div>
 							<label for="carousel-3" class="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-							<label for="carousel-2" class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label> -->
-							
+							<label for="carousel-2" class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
+						
 							<!--Slide 2-->
-							<!-- <input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
+							<input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
 							<div class="carousel-item absolute opacity-0" style="height:50vh;">
-								<div class="block h-full w-full bg-orange-500 text-white text-5xl text-center">{{$item->title}}</div>
+								<div class="block h-full w-full bg-orange-500 text-white text-5xl text-center">
+									<p>{{$outstanding[1]->title}}</p>
+									<img src="{{$outstanding[1]->picture_path}}" class="object-cover" alt="">
+								</div>
 							</div>
 							<label for="carousel-1" class="prev control-2 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
-							<label for="carousel-3" class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>  -->
-							
+							<label for="carousel-3" class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label> 
+											
 							<!--Slide 3-->
-							<!-- <input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
+							<input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
 							<div class="carousel-item absolute opacity-0" style="height:50vh;">
-								<div class="block h-full w-full bg-green-500 text-white text-5xl text-center">{{$item->title}}</div>
+								<div class="block h-full w-full bg-green-500 text-white text-5xl text-center">
+									<p>{{$outstanding[2]->title}}</p>
+									<img src="{{$outstanding[2]->picture_path}}" class="object-cover" alt="">
+								</div>
 							</div>
 							<label for="carousel-2" class="prev control-3 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
 							<label for="carousel-1" class="next control-3 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
-							@endforeach -->
-
-							<!-- Add additional indicators for each slide-->
-							<!-- @foreach($outstanding as $item)
-							
-							@endforeach
+						
+						
+						
+											<!-- Add additional indicators for each slide-->
+						<ol class="carousel-indicators">
+							<li class="inline-block mr-3">
+								<label for="carousel-1" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
+							</li>
+							<li class="inline-block mr-3">
+								<label for="carousel-2" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
+							</li>
+							<li class="inline-block mr-3">
+								<label for="carousel-3" class="carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700">•</label>
+							</li>
+						</ol>
+						
 						</div>
 					</div>
-					</div> -->
 				</section>
 
 						<!--OLD BANNER-->
