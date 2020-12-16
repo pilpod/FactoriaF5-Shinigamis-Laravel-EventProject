0de1c463f8bfaf0d2adcce397e920770ce62ad54
Alvaro <astaboada7@gmail.com>
2020-12-16 07:38:03 +0100

    slider cut out from index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -51,30 +51,11 @@
 				</section>
 
 						<!-- Banner -->
-						<section id="slider" class="slideshow-container">
-
-<!-- Full-width images with number and caption text -->
-@foreach($outstanding as $event)
-<div class="mySlides fade overflow">
-	
-	<img src="{{$event->picture_path}}" style="width:100%">
-	<div class="text">{{$event->title}}</div>
-</div>
-@endforeach
-<!-- Next and previous buttons -->
-<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-<a class="next" onclick="plusSlides(1)">&#10095;</a>
-</div>
-<br>
-
-<!-- The dots/circles -->
-<div style="text-align:center">
-@foreach($outstanding as $event)
-	<span class="dot" onclick="currentSlide('{{$event->id}}')"></span>
-@endforeach
-</div> 
-</section>
-<script src="{{ asset('js/app.js') }}"></script>
+						<div class="container">
+            				@yield('content')
+        				</div>
+						
+					
 
 			<!-- Main -->
 				<section id="main">
--- resources/views/layouts/slider.blade.php
+++ resources/views/layouts/slider.blade.php
@@ -2,27 +2,27 @@
 
 @section('slideshow')
 <!-- Slideshow container -->
-<section class="slideshow-container">
+	<section id="slider" class="slideshow-container">
 
-    <!-- Full-width images with number and caption text -->
-    @foreach($outstanding as $event)
-    <div class="mySlides fade overflow">
-        <div class="numbertext">1 / 3</div>
-        <img src="{{https://picsum.photos/id/10/2500/1667}}" style="width:100%">
-        <div class="text">{{Caption Text}}</div>
-    </div>
-    @endforeach
-    <!-- Next and previous buttons -->
+<!-- Full-width images with number and caption text -->
+        @foreach($outstanding as $event)
+            <div class="mySlides fade overflow">
+	
+	            <img src="{{$event->picture_path}}" style="width:100%">
+            <div class="text">{{$event->title}}</div>
+            </div>
+        @endforeach
+<!-- Next and previous buttons -->
     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
     <a class="next" onclick="plusSlides(1)">&#10095;</a>
 </div>
 <br>
 
 <!-- The dots/circles -->
-<div style="text-align:center">
-    <span class="dot" onclick="currentSlide(1)"></span>
-    <span class="dot" onclick="currentSlide(2)"></span>
-    <span class="dot" onclick="currentSlide(3)"></span>
+    <div style="text-align:center">
+        @foreach($outstanding as $event)
+            <span class="dot" onclick="currentSlide('{{$event->id}}')"></span>
+        @endforeach
     </div> 
 </section>
 <script src="{{ asset('js/app.js') }}"></script>
